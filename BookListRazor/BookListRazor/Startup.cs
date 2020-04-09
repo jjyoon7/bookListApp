b8369e318c6119feffe7158f2bf80ca09cc28b43
jjyoon7 <jay.yoon7@gmail.com>
2020-04-09 19:27:12 +0200

    When browser makes a https request, server access the dotnet core framework and put the request to the Context object. The Context object gets passed through the Middleware. If the Context has response along the way, then it will attach the response to the Context object and return back to the server then server will return back to the browser. Most of the time, Context object gets passed through till the end of the pipeline and returns back. This is why the order of the Middleware is important. T.ex, Authentication must comes first, so it can determine if the Context object get passed or not based on the result.

--- BookListRazor/BookListRazor/Startup.cs
+++ BookListRazor/BookListRazor/Startup.cs
@@ -43,6 +43,9 @@ namespace BookListRazor
             //these are all middlewares plugged into the pipeline when the request to the server happens.
             //it is important to remember the order, "Auth -> MVC -> Static files"
 
+            //when browser makes a https request, server access the dotnet core framework and put the request to the Context object.
+            //The Context object gets passed long through the middleware. If the Context has response along the way,
+            //then it will attach the response to the Context object and return back to the server.
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
